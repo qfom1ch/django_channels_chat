0c3f3ce89d833bd163fc0f6cc57a703d03436b76
Сергей <viip222@yandex.ru>
2023-08-27 19:40:38 +0300

    add readme

--- config/asgi.py
+++ config/asgi.py
@@ -9,8 +9,8 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 import chat.routing
